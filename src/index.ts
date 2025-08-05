31051c9e2302740888ae693c6e488d0a3ce437da
fahadmustjab <beingfahad786@gmail.com>
2025-08-05 16:30:34 +0500

    fix: core script build

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { I18nMerger } from "./core/i18nMerger";
-export { I18nFixer } from "./core/i18nFixer";
-export { I18nReporter } from "./core/i18nReporter";
-export * from "./core/types";
+export { I18nMerger } from "./core/i18nMerger.js";
+export { I18nFixer } from "./core/i18nFixer.js";
+export { I18nReporter } from "./core/i18nReporter.js";
+export * from "./core/types.js";
